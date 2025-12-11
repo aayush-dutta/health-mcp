0950c71131b0e57be1902b6253b94b5ae048db21
Aayush Dutta <aayush.dutta@gmail.com>
2025-12-10 21:47:09 -0600

    Modularize and create single score MCP endpoint. Add richer output data.

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+"""Oura ring MCP server baby..."""
+
 import os
 import json
 import threading
@@ -38,6 +40,26 @@ STATE_PATH = BASE_DIR / "oura_state.txt"
 # OURA API URL Helpers
 # -------------------
 
+def get_url_from_cat(cat: str) -> str | None:
+    """Returns associated url (extension) given the score category
+    
+    Args:
+            cat (str)   : One of "readiness","sleep" or "activity".
+    
+    Returns:
+            str|None    : The associated url extention to retrieve the score from the Oura API. 
+                            None if invalid parameter
+    
+    """
+    if cat=="readiness":
+        return "daily_readiness"
+    if cat=="sleep":
+        return "daily_sleep"
+    if cat=="activity":
+        return "daily_activity"
+
+    return None
+
 def invalid_date(dt: str)-> bool:
     """Validates whether the given string is an ISO8601 date
             Args:
@@ -45,25 +67,25 @@ def invalid_date(dt: str)-> bool:
     """
     try:
         date.fromisoformat(dt)
-    except:
+    except ValueError:
         return True
     return False
 
-def prep_dates(start_date:str, end_date:str):
+def prep_dates(start_date:str|None, end_date:str|None):
     """Performs validation on dates and returns default dates if validation fails
             Args:
-                start_date -
-                end_date   -
+                start_date (str)
+                end_date   )str
     """
     today = date.today()
-    if invalid_date(end_date) or invalid_date(start_date) is None:
+    if invalid_date(end_date) or invalid_date(start_date):
         end_date = today
         start_date=end_date-timedelta(days=6)
         end_date=end_date.isoformat()
         start_date=start_date.isoformat()
     return start_date,end_date
 
-def date_url(extn: str ,start_date:str, end_date:str):
+def date_url(extn: str ,start_date:str|None, end_date:str|None):
     """Gets any Oura API URL that requires a start date and an end date
             Args:
                 [TODO: fill in]
@@ -72,13 +94,119 @@ def date_url(extn: str ,start_date:str, end_date:str):
     return f"{OURA_API_BASE}/{extn}?start_date={start_date}&end_date={end_date}"
 
 # -------------------
+# OURA JSON Helpers
+# -------------------
+
+def process_daily_x_to_json(cat:str, data : dict) -> dict:
+    """ Processes the JSON retrieved from our call to the Oura API to desired MCP Client input
+    
+    Args:
+            cat (str)   : One of "readiness", "sleep" or "activity"
+            data (dict) : The JSON returned by our call to OURAs daily APIs
+    
+    Returns:
+            dict        : JSON input for the MCP Client
+
+    """
+    if cat == "readiness":
+        return process_daily_readiness_to_json(data)
+    if cat == "sleep":
+        return process_daily_sleep_to_json(data)
+    if cat == "activity":
+        return process_daily_activity_to_json(data)
+    
+    return {
+        "error" : "invalid category provided to retrieve daily scores"
+    }
+
+def process_daily_readiness_to_json(data : dict) -> dict:
+    """Processes OURAs readiness API to MCP Client input format
+    
+    Args:
+            data (dict) : The JSON returned by our call to OURAs daily readiness APIs
+    
+    Returns:
+            dict        : JSON readiness input for the MCP client
+
+    """
+    return {
+        "readiness" : [
+            {
+                "date" : day.get("day",None),
+                "score": day.get("score",None),
+                "contributors":day.get("contributors",{})
+            }
+            for day in data["data"]
+        ]
+    }
+
+def process_daily_sleep_to_json(data : dict) -> dict:
+    """Processes OURAs sleep API to MCP Client input format
+    
+    Args:
+            data (dict) : The JSON returned by our call to OURAs daily sleep APIs
+    
+    Returns:
+            dict        : JSON sleep input for the MCP client
+
+    """
+    return {
+        "sleep" : [
+            {
+                "date" : day.get("day",None),
+                "score": day.get("score",None),
+                "contributors":day.get("contributors",{})
+            }
+            for day in data["data"]
+        ]
+    }
+
+def process_daily_activity_to_json(data : dict) -> dict:
+    """Processes OURAs activity API to MCP Client input format
+    
+    Args:
+            data (dict) : The JSON returned by our call to OURAs daily activity APIs
+    
+    Returns:
+            dict        : JSON activity input for the MCP client
+
+    """
+    return {
+        "activity" : [
+            {
+                "date" : day.get("day",None),
+                "score": day.get("score",None),
+                "contributors":day.get("contributors",{}),
+                "active_calories":day.get("active_calories",None),
+                "target_calories":day.get("target_calories",None),
+                "total_calories":day.get("total_calories",None),
+                "steps":day.get("steps",None),
+                "sedentary_time":day.get("sedentary_time",None)
+            }
+            for day in data["data"]
+        ]
+    }
+
+# -------------------
 # TOKEN STORAGE
 # -------------------
 
 def save_token(token: dict):
+    """Saves the provided token to TOKEN_PATH
+    
+    Args:
+            token (dict) : Token to be saved locally
+    
+    """
     TOKEN_PATH.write_text(json.dumps(token))
 
 def load_token() -> dict | None:
+    """Loads the saved token from TOKEN_PATH
+    
+    Returns:
+            dict|None   : The token stored in TOKEN_PATH. None if one does not exist
+    
+    """
     if not TOKEN_PATH.exists():
         return None
     return json.loads(TOKEN_PATH.read_text())
@@ -128,7 +256,7 @@ def process_oura_callback(code: str, state: str):
 # -------------------
 async def make_oura_request(url: str) -> dict:
     """ Async wrapper for Oura request"""
-    return await asyncio.to_thread(sync_oura_get, url) 
+    return await asyncio.to_thread(sync_oura_get, url)
 
 
 def sync_oura_get(url:str) -> dict:
@@ -179,96 +307,63 @@ def get_oura_login_url() -> str:
     return authorization_url
 
 # -------------------
-# TOOL: DAILY READINESS
+# Internal OURA API Callers
+# -------------------
+
+async def internal_get_daily_x(cat:str, url_extn:str, start_date:str|None, end_date: str|None) \
+    -> dict:
+    """ Helper function score information from Oura from category cat
+    
+    """
+    url = date_url(url_extn,start_date,end_date)
+    data = await make_oura_request(url)
+
+    if not data or "data" not in data:
+        return {
+            "error" : f"Unable to fetch Oura daily {cat} summary: {data}"
+        }
+    return process_daily_x_to_json(cat,data)
+
+
+# -------------------
+# TOOL: DAILY X Score
 # -------------------
 @mcp.tool()
-async def get_oura_daily_readiness(start_date : str|None = None, end_date : str | None = None) -> dict:
-    """Fetch Oura Daily Readiness Information. Defaults to the last week's information.
+async def get_oura_daily_x_score(cat:str, start_date : str|None = None, \
+                                 end_date : str | None = None)  -> dict:
+    """Fetch Oura Daily Score related information. Defaults to the last week's information no dates.
 
         Args:
-            start_date : str - Start date of the query. Must be formatted as YYYY-MM-DD
-            end_date   : str - End date of the query. Must be formatted as YYYY-MM-DD
+                cat        (str)    : One of "readiness", "sleep" or "activity" to 
+                                        indicate which score details we want to retrieve.
+                start_date (str)    : Start date of the query. Must be formatted as YYYY-MM-DD.
+                end_date   (str)    : End date of the query. Must be formatted as YYYY-MM-DD.
+        
+        Returns:
+                dict                : MCP server output as JSON for the query
         
         Constraints:
             end_date must be after the start_date
     """
 
-    url = date_url("daily_readiness",start_date,end_date)
-    data = await make_oura_request(url)
+    url_extn = get_url_from_cat(cat)
 
-    if not data or "data" not in data:
-        return f"Unable to fetch Oura daily readiness summary: {data}"
-
-    return {
-        "readiness" : [
-            {
-                "date" : day.get("day",None),
-                "score": day.get("score",None)
-            }
-            for day in data["data"]
-        ]
-    }
-# -------------------
-# TOOL: DAILY SLEEP
-# -------------------
-@mcp.tool()
-async def get_oura_daily_sleep(start_date : str|None = None, end_date : str | None = None) -> dict:
-    """Fetch Oura Daily Sleep Information. Defaults to the last week's information.
-
-        Args:
-            start_date : str - Start date of the query. Must be formatted as YYYY-MM-DD
-            end_date   : str - End date of the query. Must be formatted as YYYY-MM-DD
-        
-        Constraints:
-            end_date must be after the start_date
-    """
-
-    url = date_url("daily_sleep",start_date,end_date)
-    data = await make_oura_request(url)
-
-    if not data or "data" not in data:
-        return f"Unable to fetch Oura daily sleep summary: {data}"
-
-    return {
-        "sleep" : [
-            {
-                "date" : day.get("day",None),
-                "score": day.get("score",None)
-            }
-            for day in data["data"]
-        ]
-    }
+    if url_extn is None:
+        return {
+            "error" : f"Invalid category: {cat}. Choose one of readiness, sleep or activity"
+        }
+    # TODO: we likely want some pagination here if the date range is too large
+    return await internal_get_daily_x(cat,url_extn,start_date,end_date)
 
 # -------------------
-# TOOL: DAILY ACTIVITY
+# TOOL: Get X Schema TODO: Get the schema for each dailies data.
 # -------------------
-@mcp.tool()
-async def get_oura_daily_activity(start_date : str|None = None, end_date : str | None = None) -> dict:
-    """Fetch Oura Daily Activity Information. Defaults to the last week's information.
-
-        Args:
-            start_date : str - Start date of the query. Must be formatted as YYYY-MM-DD
-            end_date   : str - End date of the query. Must be formatted as YYYY-MM-DD
-        
-        Constraints:
-            end_date must be after the start_date
-    """
-
-    url = date_url("daily_activity",start_date,end_date)
-    data = await make_oura_request(url)
-
-    if not data or "data" not in data:
-        return f"Unable to fetch Oura daily activity summary: {data}"
-
-    return {
-        "activity" : [
-            {
-                "date" : day.get("day",None),
-                "score": day.get("score",None)
-            }
-            for day in data["data"]
-        ]
-    }
+# -------------------
+# TOOL: TODO: Deep dive into specific things. Such as a specific days sleep
+# -------------------
+# -------------------
+# TOOL: TODO: Various other dailies.
+# -------------------
 
 # -------------------
 # CALLBACK SERVER
@@ -289,16 +384,17 @@ async def callback(request: Request):
 
 def start_callback_server():
     """Spins up callback server"""
-    uvicorn.run(app, 
-                host="127.0.0.1", 
-                port=8080, 
-                log_level="warning", 
+    uvicorn.run(app,
+                host="127.0.0.1",
+                port=8080,
+                log_level="warning",
                 access_log=False)
 
 # -------------------
 # RUN EVERYTHING
 # -------------------
 def main():
+    """Spins up callback and MCP servers"""
     # Start callback HTTP server in background thread
     threading.Thread(target=start_callback_server, daemon=True).start()
 
